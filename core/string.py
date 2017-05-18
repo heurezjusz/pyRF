1f420c467ba6d5c5c47f3fb904f41044d26e1657
Patryk Czajka <Patryk.Czajka@student.uw.edu.pl>
2017-05-18 18:43:54 +0200

    Fix error with save format

--- core/string.py
+++ core/string.py
@@ -1,9 +1,13 @@
 # functions for operating on filenames
 
+import config
 import ntpath
+from os.path import splitext
 
-def _extract_filemane(path):
-    return ntpath.basename(path)
 
 def output_filename(path, prefix=''):
-    return 'out/' + prefix + _extract_filemane(path)
+    return 'out/' + prefix + _extract_filemane(path) + '.' + config.SAVE_FORMAT
+
+
+def _extract_filemane(path):
+    return splitext(ntpath.basename(path))[0]
